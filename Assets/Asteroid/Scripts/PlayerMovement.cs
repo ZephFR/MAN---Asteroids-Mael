5c00d975f04ff1a40cb3039f6351557731e62a7d
Zeph <mael.perrot2005@gmail.com>
2026-09-08 14:48:23 +0200

    PlayerMovement.cs updated for higher refreshrate

--- Assets/Asteroid/Scripts/PlayerMovement.cs
+++ Assets/Asteroid/Scripts/PlayerMovement.cs
@@ -14,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetButton("Vertical"))
         {
